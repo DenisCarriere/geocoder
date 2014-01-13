93e4564ee26c56d9279c536cf36131b008033135
Denis Carriere <Carriere.Denis@gmail.com>
2014-01-13 01:37:18 -0500

    Updates5
    Former-commit-id: f618990395c84582946af204fe2d69571e5f4aa3

--- geocoder/__init__.py
+++ geocoder/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'geocoder'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Denis Carriere'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2014 Denis Carriere'
--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 setup(name='Geocoder',
-      version='0.1.1',
+      version='0.1.2',
       description='Python Geocoder',
       author='Denis Carriere',
       author_email='info@addxy.com',
       url='http://addxy.com',
       download_url='https://github.com/DenisCarriere/geocoder.git',
+      packages=['geocoder'],
       keywords = ['geocoder', 'geocoding', 'lat', 'lng', 'latitude', 'longitude', 'google', 'bing']
       )
