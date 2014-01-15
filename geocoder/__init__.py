476a5058ad17fe3008ab1ef962c33961514c2990
Denis Carriere <Carriere.Denis@gmail.com>
2014-01-15 17:45:06 -0500

    Couldn't call module twice, fixed now

--- geocoder/__init__.py
+++ geocoder/__init__.py
@@ -7,47 +7,41 @@ __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2014 Denis Carriere'
 
 from geocoder import Geocoder
+from google import Google
+from bing import Bing
+from maxmind import Maxmind
+from nokia import Nokia
+from esri import Esri
+from mapquest import Mapquest
+from osm import Osm
+from tomtom import Tomtom
+
 
 def google(location):
-    from google import Google
-
     return Geocoder(Google(location))
 
 def bing(location, key=''):
-    from bing import Bing
     return Geocoder(Bing(location, key))
 
 def maxmind(location):
-    from maxmind import Maxmind
-
     return ip(location)
 
 def ip(location):
     return maxmind(location)
 
 def nokia(location, app_id='', app_code=''):
-    from nokia import Nokia
-
     return Geocoder(Nokia(location, app_id, app_code))
 
 def esri(location):
-    from esri import Esri
-
     return Geocoder(Esri(location))
 
 def mapquest(location):
-    from mapquest import Mapquest
-
     return Geocoder(Mapquest(location))
 
 def osm(location):
-    from osm import Osm
-
     return Geocoder(Osm(location))
 
 def tomtom(location):
-    from tomtom import Tomtom
-
     return Geocoder(Tomtom(location))
 
 
