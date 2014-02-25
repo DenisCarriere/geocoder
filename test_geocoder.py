7ad2df235b69343051544286842002016b5d29bb
Denis Carriere <Carriere.Denis@gmail.com>
2014-02-24 21:54:49 -0500

    Added Tests

--- test_geocoder.py
+++ test_geocoder.py
@@ -1,15 +1,43 @@
 import geocoder
 import unittest
 
+location = 'Ottawa'
+ip = '74.125.226.99'
 
-class RequestsTestCase(unittest.TestCase):
+def test_entry_points():
+    geocoder.ip
+    geocoder.osm
+    geocoder.bing
+    geocoder.nokia
+    geocoder.google
+    geocoder.tomtom
+    geocoder.reverse
+    geocoder.mapquest
 
-    def test_entry_points(self):
-        geocoder.ip
-        geocoder.osm
-        geocoder.bing
-        geocoder.nokia
-        geocoder.google
-        geocoder.tomtom
-        geocoder.reverse
-        geocoder.mapquest
+def test_google():
+    g = geocoder.google(location)
+    assert g.ok
+
+def test_bing():
+    g = geocoder.bing(location)
+    assert g.ok
+
+def test_osm():
+    g = geocoder.osm(location)
+    assert g.ok
+
+def test_tomtom():
+    g = geocoder.tomtom(location)
+    assert g.ok
+
+def test_arcgis():
+    g = geocoder.arcgis(location)
+    assert g.ok
+
+def test_mapquest():
+    g = geocoder.mapquest(location)
+    assert g.ok
+
+def test_reverse():
+    g = geocoder.ip(ip)
+    assert g.ok
