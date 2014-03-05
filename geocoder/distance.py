e9c5187bab921b96f9f19d15ab8db7d72c7440b7
Denis <carriere.denis@gmail.com>
2014-03-05 15:52:04 -0500

    Location Addons
    Former-commit-id: ff45e05d2c333db00e2c02a44e60d02bb4700af9

---
+++ geocoder/distance.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from google import Google
+from geocoder import Geocoder
+from haversine import haversine
+from location import Location
+
+
+class Distance(object):
+	name = 'Distance'
+	lat1 = 0.0
+	lng1 = 0.0
+	lat2 = 0.0
+	lng2 = 0.0
+	ok = False
+	km = None
+	miles = None
+
+	def __init__(self, location1, location2):
+		self.location1 = Location(location1)
+		self.location2 = Location(location2)
+
+		# Calculate Distance
+		self.calculate_distance()
+
+		# Add address from Geocoder Class
+		self.add_address()
+
+	def __repr__(self):
+		return '<Distance - {0} to {1} [{2}km]>'.format(self.location1, self.location2, self.km)
+
+	def calculate_distance(self):
+		if bool(self.lat1 and self.lng1 and self.lat2 and self.lng2):
+			self.ok = True
+
+			latlng1 = (self.lat1, self.lng1) 
+			latlng2 = (self.lat2, self.lng2)
+			self.km = haversine(latlng1, latlng2)
+			self.miles = haversine(latlng1, latlng2, miles=True)
+		else:
+			print '<ERROR - Input is incorrect>'
+
+	def add_address(self):
+		pass
+		"""
+		if hasattr(self.location1, 'address'):
+			self.location1 = self.location1.address
+		if hasattr(self.location2, 'address'):
+			self.location2 = self.location2.address
+		"""
+
+
+if __name__ == '__main__':
+	ottawa = Geocoder(Google('Ottawa'))
+	#toronto = Geocoder(Google('Toronto')).latlng
+
+	ottawa = (45.4215296, -75.69719309999999)
+	#toronto = (43.653226, -79.3831843)
+	toronto = {'lat':43.653226, 'lng':-79.3831843}
+
+	d = Distance(ottawa, toronto)
+	print d
