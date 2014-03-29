cf524312c37d296fcbf34375ce7b74d6bed8e067
Denis <carriere.denis@gmail.com>
2014-03-29 15:10:55 -0400

    Added Proxies & Timeout

--- geocoder/tomtom.py
+++ geocoder/tomtom.py
@@ -4,18 +4,17 @@ from base import Base
 class Tomtom(Base):
     name = 'TomTom'
     url = 'https://api.tomtom.com/lbs/geocoding/geocode'
-    key = '95kjrqtpzv39ujcxfyr57wz3'
 
-    def __init__(self, location, key=''):
+    def __init__(self, location, key):
         self.location = location
-        if not key:
-            key = self.key
         self.json = dict()
         self.params = dict()
         self.params['key'] = key
         self.params['query'] = location
         self.params['format'] = 'json'
         self.params['maxResults'] = 1
+        if not key:
+            self.help_key()
 
     def lat(self):
         return self.safe_coord('geoResult-latitude')
@@ -40,3 +39,13 @@ class Tomtom(Base):
 
     def country(self):
         return self.safe_format('geoResult-country')
+
+    def help_key(self):
+        print '<ERROR> Please provide a Key paramater when using TomTom'
+        print '>>> import geocoder'
+        print '>>> key = "XXXX"'
+        print '>>> g = geocoder.tomtom(<location>, key=key)'
+        print ''
+        print 'How to get a Key?'
+        print '-----------------'
+        print 'http://developer.tomtom.com/products/geocoding_api'
--- test_geocoder.py
+++ test_geocoder.py
@@ -8,6 +8,11 @@ ip = '74.125.226.99'
 repeat = 3
 ottawa = (45.4215296, -75.6971930)
 toronto = (43.653226, -79.3831843)
+bing_key = 'AtnSnX1rEHr3yTUGC3EHkD6Qi3NNB-PABa_F9F8zvLxxvt8A7aYdiG3bGM_PorOq'
+tomtom_key = '95kjrqtpzv39ujcxfyr57wz3'
+app_id = '6QqTvc3kUWsMjYi7iGRb'
+app_code = 'q7R__C774SunvWJDEiWbcA'
+username = 'addxy'
 
 def test_entry_points():
     geocoder.ip
@@ -30,7 +35,14 @@ def test_google():
 
 def test_bing():
     for i in xrange(repeat):
-        g = geocoder.bing(location)
+        g = geocoder.bing(location, key=bing_key)
+        if g.ok:
+            return True
+    return False
+
+def test_nokia():
+    for i in xrange(repeat):
+        g = geocoder.nokia(location, app_code=app_code, app_id=app_id)
         if g.ok:
             return True
     return False
@@ -44,7 +56,7 @@ def test_osm():
 
 def test_tomtom():
     for i in xrange(repeat):
-        g = geocoder.tomtom(location)
+        g = geocoder.tomtom(location, key=tomtom_key)
         if g.ok:
             return True
     return False
@@ -65,7 +77,7 @@ def test_mapquest():
 
 def test_geonames():
     for i in xrange(repeat):
-        g = geocoder.geonames(location)
+        g = geocoder.geonames(location, username=username)
         if g.ok:
             return True
     return False
