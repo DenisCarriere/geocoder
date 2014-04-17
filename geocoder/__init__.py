60dd3ea8368d5a090d41eb1f00f045e997b2cba9
Denis Carriere <Carriere.Denis@gmail.com>
2014-04-17 18:23:43 -0400

    Minor text edits

--- geocoder/__init__.py
+++ geocoder/__init__.py
@@ -20,7 +20,7 @@ Geocoder is an Apache2 Licensed Geocoding library, written in Python.
 """
 
 __title__ = 'geocoder'
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __author__ = 'Denis Carriere'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2014 Denis Carriere'
