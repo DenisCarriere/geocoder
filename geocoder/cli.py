0b718ae77ae502ea6a02b1c7cfbcff8a6b487506
Antonio Lima <anto87@gmail.com>
2014-10-08 08:15:08 -0400

    Reintroduced 'geocode' single address
    Former-commit-id: 8a1fa3222f9d466234581ca3dd1d0e9cd1193307

--- geocoder/cli.py
+++ geocoder/cli.py
@@ -6,15 +6,26 @@ import click
 import geocoder
 
 @click.command()
-@click.argument('infile', type=click.File('r'), default=sys.stdin)
-@click.option('--provider', default='bing', help="Provider (choose from: bing,\
-geonames, google, mapquest, nokia, osm, tomtom, geolytica, arcgis, yahoo")
+@click.argument('input', type=str, default="")
+@click.option('--provider', '-p', default='bing', help="provider (choose from: bing,\
+geonames, google, mapquest, nokia, osm, tomtom, geolytica, arcgis, yahoo)")
 @click.option('--outfile', '-o', type=click.File('w'), default=sys.stdout,
         help="Output File")
-def cli(infile, provider, outfile):
-    """Geocode multiple strings, line by line, passed via STDIN or contained in a file."""
+def cli(input, provider, outfile):
+    """Geocode one or many string.
+
+    Arguments:
+
+        input      string or filename containing one string per line.
+
+    """
+    try:
+        input = open(input, "r")
+    except:
+        input = [input]
+
     # Loop inside the reader
-    for item in infile:
+    for item in input:
         item = item.strip()
         g = geocoder.geocode(item, provider=provider)
         outfile.write("{}\n".format(json.dumps(g.json)))
