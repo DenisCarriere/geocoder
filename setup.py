0544c8c7db3250f5ac200f0b9a021a3c5dd5ae0e
DenisCarriere <carriere.denis@gmail.com>
2014-12-04 23:06:41 -0500

    Update version 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist bdist_wheel upload')
     sys.exit()
 
-version = '1.0.0'
+version = '1.0.1'
 requires = ['requests>=2.3.0', 'xmltodict>=0.9.0']
 
 with open('README.md') as f:
