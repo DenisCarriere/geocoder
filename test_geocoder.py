5500f2fcd628eeb96170f4c1c2d0d5db8dd8c7ed
DenisCarriere <carriere.denis@gmail.com>
2015-02-28 14:17:00 -0500

    Bing first request rarely successful

--- test_geocoder.py
+++ test_geocoder.py
@@ -70,12 +70,16 @@ def test_google_elevation():
 
 
 def test_bing():
+    # First request rarely successful
+    g = geocoder.bing(location)
     g = geocoder.bing(location)
     assert g.ok
     assert g.city == city
 
 
 def test_bing_reverse():
+    # First request rarely successful
+    g = geocoder.bing(ottawa, method='reverse')
     g = geocoder.bing(ottawa, method='reverse')
     assert g.ok
 
