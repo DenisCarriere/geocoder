5de61a27a552b91de2c80bb3ad16184985f045c7
Denis Carriere <carriere.denis@gmail.com>
2015-03-17 13:43:22 -0400

    Added version detection function
    Inspired by Scott:
    
    http://locallyoptimal.com/blog/2014/03/14/building-a-basic-package-pt-1-bare-bones/

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python
 # coding: utf8
 from setuptools import setup
+import os
+import codecs
+import re
 
-__version__ = '1.2.4'
+
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 requirements_file = "requirements.txt"
 requirements = [pkg.strip() for pkg in open(requirements_file).readlines()]
 
@@ -10,11 +28,12 @@ try:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst') + "\n"
 except(IOError, ImportError):
-    long_description = open('README.md').read() + "\n"
+    with codecs.open('README.md', encoding='utf-8') as f:
+        long_description = f.read() + "\n"
 
 setup(
     name='geocoder',
-    version=__version__,
+    version=find_version('geocoder', '__init__.py'),
     description="Geocoder is a geocoding library, written in python,"
                 " simple and consistent.",
     long_description=long_description,
