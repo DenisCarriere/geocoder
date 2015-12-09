c9302289b68a9ae52167276c7d117f3b94cf79c0
Denis <carriere.denis@gmail.com>
2015-12-09 12:31:48 -0500

    Refactored Komoot Reverse
    - Inheriting the Komoot class allows you to use all the existing
    `@property`.
    - The `Location` class does all the parsing for loading a reverse
    geocode.
    @stirringhalo

--- geocoder/komoot_reverse.py
+++ geocoder/komoot_reverse.py
@@ -2,11 +2,11 @@
 # coding: utf8
 
 from __future__ import absolute_import
-from geocoder.base import Base
 from geocoder.komoot import Komoot
 from geocoder.location import Location
 
-class KomootReverse(Base):
+
+class KomootReverse(Komoot):
     """
     Komoot REST API
     =======================
@@ -19,60 +19,22 @@ class KomootReverse(Base):
     method = 'reverse'
 
     def __init__(self, location, **kwargs):
-        t = location.split()
-        print t
         self.url = 'https://photon.komoot.de/reverse'
+        self.location = location
+        location = Location(location)
         self.params = {
-            'lat': t[0],
-            'lon': t[1],
+            'lat': location.lat,
+            'lon': location.lng,
         }
         self._initialize(**kwargs)
 
     def _exceptions(self):
         if self.parse['features']:
-            self._build_tree(self.parse['features'][0]['geometry'])
-            self._build_tree(self.parse['features'][0]['properties'])
             self._build_tree(self.parse['features'][0])
 
     @property
-    def lat(self):
-        return self.parse['coordinates'][1]
-
-    @property
-    def lng(self):
-        return self.parse['coordinates'][0]
-
-    @property
-    def address(self):
-        def xstr(s):
-            if s is None:
-                return ''
-            return unicode(s) + " "
-        return xstr((self.parse['properties'].get('housenumber'))) + xstr((self.parse['properties'].get('street'))) + xstr((self.parse['properties'].get('city'))) + xstr((self.parse['properties'].get('state'))) + xstr((self.parse['properties'].get('country'))) + xstr((self.parse['properties'].get('postcode')))
-
-    @property
-    def country(self):
-        return self.parse['properties'].get('country')
-
-    @property
-    def state(self):
-         return self.parse['properties'].get('state')
-
-    @property
-    def city(self):
-         return self.parse['properties'].get('city')
-
-    @property
-    def street(self):
-         return self.parse['properties'].get('street')
-
-    @property
-    def housenumber(self):
-         return self.parse['properties'].get('housenumber')
-
-    @property
-    def postal(self):
-         return self.parse['properties'].get('postcode')
+    def ok(self):
+        return bool(self.address)
 
 if __name__ == '__main__':
     g = KomootReverse("45.4 -75.7")
