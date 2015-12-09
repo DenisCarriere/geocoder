f30229444c50d93553e7497e91a711a2a514c3d5
Denis <carriere.denis@gmail.com>
2015-12-09 12:24:57 -0500

    Refactoring Komoot
    - Refactored most of the properties
    - Added osm properties
    - Added for iter loop for additional data (not getting parsed by
    Geocoder)
    ```
    g = geocoder.komoot('Ottawa, ON')
    for item in g:
    print item
    ```
    @stirringhalo

--- geocoder/komoot.py
+++ geocoder/komoot.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import
 from geocoder.base import Base
 
+
 class Komoot(Base):
     """
     Komoot REST API
@@ -19,82 +20,107 @@ class Komoot(Base):
     def __init__(self, location, **kwargs):
         self.url = 'http://photon.komoot.de/api'
         self.location = location
-        if 'result' in kwargs:
-            if kwargs['result']:
-                limit = kwargs['result']
-        else:
-            size = 1
         self.params = {
             'q': location,
-            'limit': limit,
+            'limit': kwargs.get('result', 1),
             'lang': 'en',
         }
         self._initialize(**kwargs)
 
     def _exceptions(self):
-        self._build_tree(self.parse['geometry'])
-        self._build_tree(self.parse['properties'])
+        # Only retrieve the first feature
+        features = self.parse['features']
+        if features:
+            self._build_tree(self.parse['features'][0])
 
-    def next(self):
+    def __iter__(self):
         for item in self.content['features']:
             yield item
 
     @property
     def lat(self):
-        return self.parse['coordinates'][1]
+        return self.parse['geometry']['coordinates'][1]
 
     @property
     def lng(self):
-        return self.parse['coordinates'][0]
+        return self.parse['geometry']['coordinates'][0]
+
+    @property
+    def bbox(self):
+        extent = self.parse['properties']['extent']
+        if extent:
+            west = extent[0]
+            north = extent[1]
+            east = extent[2]
+            south = extent[3]
+            return self._get_bbox(south, west, north, east)
 
     @property
     def address(self):
-        def xstr(s):
-            if s is None:
-                return ''
-            return unicode(s) + " "
-        return xstr((self.parse['properties'].get('housenumber'))) + xstr((self.parse['properties'].get('street'))) + xstr((self.parse['properties'].get('city'))) + xstr((self.parse['properties'].get('state'))) + xstr((self.parse['properties'].get('country'))) + xstr((self.parse['properties'].get('postcode')))
+        # Ontario, Canada
+        address = ', '.join([self.state, self.country])
+
+        # 453 Booth street, Ottawa ON, Canada
+        if self.housenumber:
+            middle = ', '.join([self.street, self.city])
+            address = ' '.join([self.housenumber, middle, address])
+
+        # 453 Booth street, Ottawa ON, Canada
+        elif self.street:
+            middle = ', '.join([self.street, self.city])
+            address = ' '.join([middle, address])
+
+        # Ottawa ON, Canada
+        elif self.city:
+            address = ' '.join([self.city, address])
+
+        return address
 
     @property
     def country(self):
-         return self.parse['properties'].get('country')
+        return self.parse['properties'].get('country', '')
 
     @property
     def state(self):
-         return self.parse['properties'].get('state')
+        if self.osm_value == 'state':
+            return self.parse['properties'].get('name', '')
+        return self.parse['properties'].get('state', '')
 
     @property
     def city(self):
-         return self.parse['properties'].get('city')
+        if self.osm_value == 'city':
+            return self.parse['properties'].get('name', '')
+        return self.parse['properties'].get('city', '')
 
     @property
     def street(self):
-         return self.parse['properties'].get('street')
+        return self.parse['properties'].get('street', '')
 
     @property
     def housenumber(self):
-         return self.parse['properties'].get('housenumber')
+        return self.parse['properties'].get('housenumber', '')
 
     @property
     def postal(self):
-         return self.parse['properties'].get('postcode')    
+        return self.parse['properties'].get('postcode', '')
+
+    @property
+    def osm_id(self):
+        return self.parse['properties'].get('osm_id', '')
+
+    @property
+    def osm_value(self):
+        return self.parse['properties'].get('osm_value', '')
+
+    @property
+    def osm_key(self):
+        return self.parse['properties'].get('osm_key', '')
+
+    @property
+    def osm_type(self):
+        return self.parse['properties'].get('osm_type', '')
 
 if __name__ == '__main__':
-    g = Komoot('Toronto',result=1)
-    print " "
-    print g.json
-    g = Komoot('Toronto',result=2)
-    print " "
-    print g.json
-    g = Komoot('Toronto',result=3)
-    print " "
-    print g.json
-    g = Komoot('Toronto',result=4)
-    print " "
-    print g.json
-    g = Komoot('Burj Khalifa',result=1)
-    print " "
-    print g.json
-    g = Komoot('52 Northview Road',result=1)
-    print " "
-    print g.json
+    g = Komoot('Ottawa Ontario', result=3)
+    for item in g:
+        print item
