dafa33292c9eb1a89c4c6cee2dd908847c9317a2
ebreton <manu@ibimus.com>
2017-07-26 12:53:58 +0200

    Changed import to be compatible with python 2.7

--- tests/test_session.py
+++ tests/test_session.py
@@ -2,9 +2,12 @@
 # coding: utf8
 
 import geocoder
-from unittest import mock
 import requests
 
+try:
+    import mock
+except ImportError:
+    from unittest import mock
 
 address = 'Booth Street, Ottawa'
 
