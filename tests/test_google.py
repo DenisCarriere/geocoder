887e4b4065a344f0e81b514a3f75864261e371ab
ebreton <manu@ibimus.com>
2017-08-01 10:14:41 +0200

    removed google mocks that fails during Travis build

--- tests/test_google.py
+++ tests/test_google.py
@@ -28,18 +28,8 @@ def test_google():
 
 
 def test_google_reverse():
-    urls = [
-        # when testing locally
-        'https://maps.googleapis.com/maps/api/geocode/json?language=&latlng=45.4215296%2C+-75.697193&sensor=false&key=mock',
-        # when building in Travis (secured connection implies ordered parameters)
-        'https://maps.googleapis.com/maps/api/geocode/json?client=[secure]&latlng=45.4215296%2C+-75.697193&sensor=false&signature=iXbq6odmrYN0XgcfB5EPcgEvR-I%3D',
-    ]
-    data_file = 'tests/results/google_reverse.json'
-    with requests_mock.Mocker() as mocker, open(data_file, 'r') as input:
-        for url in urls:
-            mocker.get(url, text=input.read())
-        g = geocoder.google(ottawa, method='reverse', key='mock')
-        assert g.ok
+    g = geocoder.google(ottawa, method='reverse')
+    assert g.ok
 
 
 # FIXME from ebreton: what difference with test_google() ?
@@ -55,18 +45,8 @@ def test_google_reverse():
 
 
 def test_google_timezone():
-    urls = [
-        # when testing locally
-        'https://maps.googleapis.com/maps/api/timezone/json?location=45.4215296%2C+-75.697193&timestamp=1501571979.087397',
-        # when building in Travis (secured connection implies ordered parameters)
-        'https://maps.googleapis.com/maps/api/timezone/json?timestamp=1501571979.09&location=45.4215296%2C+-75.697193'
-    ]
-    data_file = 'tests/results/google_timezone.json'
-    with requests_mock.Mocker() as mocker, open(data_file, 'r') as input:
-        for url in urls:
-            mocker.get(url, text=input.read())
-        g = geocoder.google(ottawa, method='timezone', timestamp=1501571979.087397)
-        assert g.ok
+    g = geocoder.google(ottawa, method='timezone')
+    assert g.ok
 
 
 # def test_google_elevation():
