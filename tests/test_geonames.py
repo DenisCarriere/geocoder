b70e7ce54f907f6255e95602990da9eb9cc59656
ebreton <manu@ibimus.com>
2017-08-01 09:22:55 +0200

    added mocks for google

--- tests/test_geonames.py
+++ tests/test_geonames.py
@@ -1,6 +1,5 @@
 # coding: utf8
 
-import json
 import geocoder
 
 import requests_mock
--- tests/test_google.py
+++ tests/test_google.py
@@ -3,6 +3,8 @@
 
 import geocoder
 
+import requests_mock
+
 address = 'The Happy Goat, Ottawa'
 location = 'Ottawa, Ontario'
 city = 'Ottawa'
@@ -10,20 +12,33 @@ ottawa = (45.4215296, -75.6971930)
 
 
 def test_google():
-    g = geocoder.google(location, client=None)
-    assert g.ok
-    assert str(g.city) == city
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?language=&address=Ottawa,%20Ontario&bounds=&components=&region=&key=mock'
+    data_file = 'tests/results/google.json'
+    with requests_mock.Mocker() as mocker, open(data_file, 'r') as input:
+        mocker.get(url, text=input.read())
+        g = geocoder.google(location, client=None, key='mock')
+        assert g.ok
+        assert str(g.city) == city
 
 
 def test_google_reverse():
-    g = geocoder.google(ottawa, method='reverse')
-    assert g.ok
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?language=&latlng=45.4215296%2C+-75.697193&sensor=false&key=mock'
+    data_file = 'tests/results/google_reverse.json'
+    with requests_mock.Mocker() as mocker, open(data_file, 'r') as input:
+        mocker.get(url, text=input.read())
+        g = geocoder.google(ottawa, method='reverse', key='mock')
+        assert g.ok
 
 
 def test_google_for_work():
-    g = geocoder.google(location)
-    assert g.ok
-    assert str(g.city) == city
+    # FIXME from ebreton: what difference with test_google() ?
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?language=&address=Ottawa%2C+Ontario&bounds=&components=&region=&key=mock'
+    data_file = 'tests/results/google.json'
+    with requests_mock.Mocker() as mocker, open(data_file, 'r') as input:
+        mocker.get(url, text=input.read())
+        g = geocoder.google(location, key='mock')
+        assert g.ok
+        assert str(g.city) == city
 
 
 # def test_google_places():
@@ -32,8 +47,12 @@ def test_google_for_work():
 
 
 def test_google_timezone():
-    g = geocoder.google(ottawa, method='timezone')
-    assert g.ok
+    url = 'https://maps.googleapis.com/maps/api/timezone/json?location=45.4215296%2C+-75.697193&timestamp=1501571979.087397'
+    data_file = 'tests/results/google_timezone.json'
+    with requests_mock.Mocker() as mocker, open(data_file, 'r') as input:
+        mocker.get(url, text=input.read())
+        g = geocoder.google(ottawa, method='timezone', timestamp=1501571979.087397)
+        assert g.ok
 
 
 # def test_google_elevation():
