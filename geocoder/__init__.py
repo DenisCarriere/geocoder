8948e063ab304a2b6311a947a16e59251c4a5357
Denis Carriere <carriere.denis@gmail.com>
2017-08-14 18:40:47 +0000

    Bump minor release

--- geocoder/__init__.py
+++ geocoder/__init__.py
@@ -28,7 +28,7 @@ Consistant JSON responses from various providers.
 __title__ = 'geocoder'
 __author__ = 'Denis Carriere'
 __author_email__ = 'carriere.denis@gmail.com'
-__version__ = '1.26.0'
+__version__ = '1.27.0'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2013-2016 Denis Carriere'
 
