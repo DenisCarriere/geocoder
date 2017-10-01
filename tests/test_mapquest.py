da88500fbf2142e865e2d9b0ec83aeaf9aa89644
ebreton <manu@ibimus.com>
2017-10-01 10:12:22 +0200

    extend timeout for mapquest in tests

--- tests/test_mapquest.py
+++ tests/test_mapquest.py
@@ -9,7 +9,7 @@ ottawa = (45.50, -76.05)
 
 
 def test_mapquest():
-    g = geocoder.mapquest(location)
+    g = geocoder.mapquest(location, timeout=10)
     assert g.ok
     osm_count, fields_count = g.debug()[0]
     assert osm_count == 3
@@ -17,10 +17,10 @@ def test_mapquest():
 
 
 def test_mapquest_reverse():
-    g = geocoder.mapquest(ottawa, method='reverse')
+    g = geocoder.mapquest(ottawa, method='reverse', timeout=10)
     assert g.ok
 
 
 def test_multi_results():
-    g = geocoder.mapquest(location, maxRows=3)
+    g = geocoder.mapquest(location, maxRows=3, timeout=10)
     assert len(g) == 3
