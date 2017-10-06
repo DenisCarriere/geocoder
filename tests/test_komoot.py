1a0f80101bec927fd1e9f756808dd770a5212db9
ebreton <manu@ibimus.com>
2017-10-06 23:17:45 +0200

    increase timeout for komoot

--- tests/test_komoot.py
+++ tests/test_komoot.py
@@ -8,7 +8,7 @@ ottawa = (45.4215296, -75.6971930)
 
 
 def test_komoot():
-    g = geocoder.komoot(location)
+    g = geocoder.komoot(location, timeout=10)
     assert g.ok
     assert len(g) == 1
     osm_count, fields_count = g.debug()[0]
@@ -17,11 +17,11 @@ def test_komoot():
 
 
 def test_komoot_multi_result():
-    g = geocoder.komoot(location, maxRows=3)
+    g = geocoder.komoot(location, maxRows=3, timeout=10)
     assert g.ok
     assert len(g) == 3
 
 
 def test_komoot_reverse():
-    g = geocoder.komoot(ottawa, method='reverse')
+    g = geocoder.komoot(ottawa, method='reverse', timeout=10)
     assert g.ok
