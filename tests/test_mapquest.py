ffccf4e6136bc6f1c9490db7b9121da1623bda11
Thomas Labadie <thomas.labadie@gmail.com>
2018-02-07 14:15:59 +0100

    MapQuest: batch feature unit testing

--- tests/test_mapquest.py
+++ tests/test_mapquest.py
@@ -6,6 +6,7 @@ import geocoder
 location = 'Ottawa'
 city = 'Ottawa'
 ottawa = (45.50, -76.05)
+locations = ['Denver,CO', 'Boulder,CO']
 
 
 def test_mapquest():
@@ -20,6 +21,10 @@ def test_mapquest_reverse():
     g = geocoder.mapquest(ottawa, method='reverse', timeout=10)
     assert g.ok
 
+def test_mapquest_batch():
+    g = geocoder.mapquest(locations, method='batch', timeout=10)
+    assert g.ok
+    assert len(g) == 2
 
 def test_multi_results():
     g = geocoder.mapquest(location, maxRows=3, timeout=10)
